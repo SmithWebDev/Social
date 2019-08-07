e0e8002e25bcb9231715834cda6f9c659d5f077c
Smith <smithwebdd@gmail.com>
2019-08-07 09:32:36 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'epicenter/feed'
-  get 'epicenter/show_user'
-  get 'epicenter/now_following'
-  get 'epicenter/unfollow'
+  root 'epicenter#feed'
+  get 'show_user'=> 'epicenter#show_user'
+  get 'now_following'=> 'epicenter#now_following'
+  get 'unfollow'=> 'epicenter#unfollow'
   resources :tweets
   devise_for :users
 
-  root 'tweets#index'
+  
 end
