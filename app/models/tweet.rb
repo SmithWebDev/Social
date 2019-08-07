688fa3332dedc1de90cf7e83ba6ffdbd27643c16
Smith <smithwebdd@gmail.com>
2019-08-07 08:42:24 -0500

    added username validations, tweet validations, and global navbar

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
+  validates :message, presence: true
+  validates :message, length: {maximum: 140, too_long: "A tweet is only 140 max"}
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
   has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
