2e21b2f71ba8c1bbf5a6e3ffa62613ec51103dd5
Smith <smithwebdd@gmail.com>
2019-08-08 08:56:19 -0500

    created link parameter for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -71,6 +71,6 @@ class TweetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:message, :user_id)
+      params.require(:tweet).permit(:message, :user_id, :link)
     end
 end
