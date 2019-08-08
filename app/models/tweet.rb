adaccfc00f16f06a4702bb052873b2f44d0e98db
Smith <smithwebdd@gmail.com>
2019-08-08 09:59:57 -0500

    completed link shortening for tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,41 @@ class Tweet < ApplicationRecord
   has_many :tweet_tags
   has_many :tags, through: :tweet_tags
   
+  before_validation :link_check, on: :create
   validates :message, presence: true
-  validates :message, length: {maximum: 140, too_long: "A tweet is only 140 max"}
+  validates :message, length: {maximum: 140, too_long: "A tweet is only 140 max"}, on: :create
+  after_validation :apply_link, on: :create
+  
+  private
+  def link_check
+    check = false
+    if self.message.include? "http://"
+      check = true
+    elsif self.message.include? "https:??"
+      check = true
+    else
+      check = false
+    end
+
+    if check == true
+      arr = self.message.split
+      index = arr.map{|x| x.include? "http"}.index(true)
+      self.link = arr[index]
+      if arr[index].length > 23
+        arr[index] = "#{arr[index][0..20]}"
+      end
+      self.message = arr.join(" ")
+    end
+  end
+
+  def apply_link
+    arr = self.message.split
+    index = arr.map { |x| x.include?("http://") || x.include?("https://")}.index(true)
+    if index
+      url = arr[index]
+      arr[index] = "<a href='#{self.link}' target='_blank'>#{url}></a>"
+    end
+    self.message = arr.join(" ")
+  end
+
 end
