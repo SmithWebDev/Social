43fe5659646f64a985abf33e6f3900651ef12bfa
Smith <smithwebdd@gmail.com>
2019-08-08 08:31:55 -0500

    add function to make hashtag a clickable link

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,7 +28,7 @@ class TweetsController < ApplicationController
 
     message_arr = @tweet.message.split
 
-    message_arr.each do |word|
+    message_arr.each_with_index do |word, index|
       if word[0] == "#"
         if Tag.pluck(:phrase).include?(word)
           tag = Tag.find_by(phrase: word)
@@ -36,6 +36,7 @@ class TweetsController < ApplicationController
           tag = Tag.create(phrase: word)
         end
         tweet_tag = TweetTag.create(tweet_id: @tweet.id, tag_id: tag.id)
+        message_arr[index] = "<a href='/tag_tweets?id=#{tag.id}'>#{word}</a>"
       end
     end
 
